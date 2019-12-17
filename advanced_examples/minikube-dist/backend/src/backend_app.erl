4c5553caa09daf3e5ea6599818896260d4980aa3
Lukas Larsson <lukas@erlang.org>
2019-12-17 16:12:47 +0100

    Fix fix dist example

--- advanced_examples/minikube-dist/backend/src/backend_app.erl
+++ advanced_examples/minikube-dist/backend/src/backend_app.erl
@@ -12,11 +12,11 @@
 -export([start/2,stop/1]).
 
 -include_lib("kernel/include/inet.hrl").
+-include_lib("kernel/include/logger.hrl").
 
 %% API.
 
 start(_Type, _Args) ->
-    os:set_signal(sigchld,ignore),
     connect_db_nodes(),
     dockerwatch_sup:start_link().
 
@@ -25,8 +25,22 @@ stop(_State) ->
 
 %% We lookup the k8s service through dns and connect to all backend nodes
 connect_db_nodes() ->
-    {ok, SVC} = application:get_env(backend, service),
-    {ok,#hostent{ h_name = FQDN }} = inet:gethostbyname(SVC),
+    FQDN = get_service(),
     Nodes = [list_to_atom("dockerwatch@"++Host) ||
                 {_Prio,_Weight,_Port,Host} <- inet_res:lookup(FQDN,any,srv)],
     [net_adm:ping(N) || N <- Nodes].
+
+get_service() ->
+    {ok, SVC} = application:get_env(backend, service),
+    get_service(SVC,0,10).
+get_service(SVC,N,M) ->
+    case inet:gethostbyname(SVC) of
+        {ok,#hostent{ h_name = FQDN }} ->
+            FQDN;
+        {error,nxdomain} when N =/= M ->
+            ?LOG_INFO("Failed to get service, sleeping and then retrying"),
+            timer:sleep(N*N * 100),
+            get_service(SVC,N+1,M);
+        Res ->
+            Res
+    end.
