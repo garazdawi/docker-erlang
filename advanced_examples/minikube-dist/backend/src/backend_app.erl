50ad57e327b7844366a1d717b20c3c95b02da320
Lukas Larsson <lukas@erlang.org>
2019-12-19 10:32:13 +0100

    Revert "Try to use erl_epmd:names to setup cluster"
    This reverts commit 55e7af483a7c74a45893811866b4042963738a78.

--- advanced_examples/minikube-dist/backend/src/backend_app.erl
+++ advanced_examples/minikube-dist/backend/src/backend_app.erl
@@ -23,8 +23,24 @@ start(_Type, _Args) ->
 stop(_State) ->
     ok.
 
-%% This will call erl_epmd_dns_srv:names(SRV) and then do net_adm:ping to
-%% all nodes in the world list.
+%% We lookup the k8s service through dns and connect to all backend nodes
 connect_db_nodes() ->
+    FQDN = get_service(),
+    Nodes = [list_to_atom("dockerwatch@"++Host) ||
+                {_Prio,_Weight,_Port,Host} <- inet_res:lookup(FQDN,any,srv)],
+    [net_adm:ping(N) || N <- Nodes].
+
+get_service() ->
     {ok, SVC} = application:get_env(backend, service),
-    net_kernel:world_list(SRV).
+    get_service(SVC,0,10).
+get_service(SVC,N,M) ->
+    case inet:gethostbyname(SVC) of
+        {ok,#hostent{ h_name = FQDN }} ->
+            FQDN;
+        {error,nxdomain} when N =/= M ->
+            ?LOG_INFO("Failed to get service, sleeping and then retrying"),
+            timer:sleep(N*N * 100),
+            get_service(SVC,N+1,M);
+        Res ->
+            Res
+    end.
